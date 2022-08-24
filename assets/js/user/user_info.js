644efae8b3d42d682168520791a9ddb1907cf15f
ithyx <2449030152@qq.com>
2022-08-24 17:32:57 +0800

    保存user

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -20,10 +20,38 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg('获取用户信息失败！')
                 }
-                console.log(res)
+                // console.log(res)
                 form.val('formUserInfo', res.data)
             }
 
         })
     }
+    // 重置表单数据
+    $('#btnReset').on('click', function (e) {
+        e.preventDefault()
+        //初始化用户的基本信息
+        initUserInfo()
+    })
+
+    // 监听表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('修改用户信息成功！')
+                // console.log(res);
+                // //调用父页面的方法 重新渲染用户头像和用户信息---
+                // 1.$(function())会形成局部作用域，
+                // 2.我们声明函数时，最好把其他页面也需要用到的函数 声明在全局作用域中 以便 子页面 调用
+                window.parent.getUserInfo()
+            }
+        })
+
+    })
 })
